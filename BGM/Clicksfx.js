99b3887268a8a62871baab13ee46fc001525ca8a
VuittonVU <145370052+VuittonVU@users.noreply.github.com>
2024-11-09 19:23:52 +0700

    Delete BGM directory

--- BGM/Clicksfx.js
+++
@@ -1,8 +0,0 @@
-const clickSound = document.getElementById("click-sound");
-
-
-document.addEventListener("click", function () {
-    // Play the sound
-    clickSound.currentTime = 0; // Reset time to start the sound from the beginning
-    clickSound.play();
-  });

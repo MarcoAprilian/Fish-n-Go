2914b85c5c33a1ec94599edf2d7821132756985f
VuittonVU <145370052+VuittonVU@users.noreply.github.com>
2024-11-21 11:29:34 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$port = '5432';
+$user = 'postgres';
+$password = 'password';
+$dbname = 'Register';
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    exit();
+}
+?>

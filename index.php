4e7717bab14a4c91ab44874bff5e9f1258b22d5d
VuittonVU <145370052+VuittonVU@users.noreply.github.com>
2024-11-29 11:08:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,37 +18,30 @@ session_start();
     <canvas id="bubblesCanvas"></canvas>
     <div class="container">
         <div class="top-right-buttons">
-            
             <a href="About/about.html">
-                <button class="about-button">About</button>
+                <button class="about-button">Cara Bermain</button>
             </a>
-
             <a href="leaderboard/leaderboard.php">
-                <button class="leaderboard-button">Leader Board</button>
+                <button class="leaderboard-button">Papan Peringkat</button>
             </a>
-
-            <?php if (isset($_SESSION['username'])): ?>
-            <a href="Profil/profil.html">
-                <button class="login-button"><?php echo $_SESSION['username']; ?></button>
-            </a>
-            <?php else: ?>
-            <a href="Signin/halamansignin.php">
-                <button class="login-button">Log In</button>
-            </a>
-            <?php endif; ?>
         </div>
-
         <img src="Image/logo.png" alt="Logo" class="logo">
         <p class="fade-in title">FISH n' GO</p>
-
-        <div class="play-button-container">
-            <button class="center-button button1" id="play-button">PLAY</button>
-        </div>
-
+        <?php if (isset($_SESSION['username'])): ?>
+            <div class="center-container">
+                <button class="center-button" id="play-button">MAIN</button>
+                <a href="Profil/profil.php">
+                    <button class="center-button">PROFIL</button>
+                </a>
+            </div>
+        <?php else: ?>
+            <a href="Signin/halamansignin.php">
+                <button class="center-button">Masuk</button>
+            </a>
+        <?php endif; ?>
         <a href="Us/us.html">
-            <button class="us-button">About Us</button>
+            <button class="us-button">Tentang Kami</button>
         </a>
-
         <div class="music-button">
             <div class="dropdown">
                 <button id="menu-button">
@@ -56,22 +49,21 @@ session_start();
                 </button>
                 <div class="dropdown-content" id="dropdown-content">
                     <p>Track List</p>
-                    <button id="play-1" value="/BGM/BGM1.mp3"><i class="fa fa-play spasi"></i>Track 1<br></button> 
-                    <button id="play-2" value="/BGM/BGM2.mp3"><i class="fa fa-play spasi"></i>Track 2<br></button> 
+                    <button id="play-1" value="/BGM/BGM1.mp3"><i class="fa fa-play spasi"></i>Track 1<br></button>
+                    <button id="play-2" value="/BGM/BGM2.mp3"><i class="fa fa-play spasi"></i>Track 2<br></button>
                     <button id="play-3" value="/BGM/BGM3.mp3"><i class="fa fa-play spasi"></i> Track 3<br></button>
                     <button id="play-4" value="/BGM/BGM4.mp3"><i class="fa fa-play spasi"></i> Track 4<br></button>
-                    <button id="play-5" value="/BGM/BGMMIAW.mp3"> <i class="fa fa-play spasi"></i>Track 5<br></button> 
+                    <button id="play-5" value="/BGM/BGMMIAW.mp3"> <i class="fa fa-play spasi"></i>Track 5<br></button>
                     <button id="mute">Mute</button>
                 </div>
             </div>
         </div>
     </div>
-
     <audio id="click-sound" src="BGM/mouse.mp3" preload="auto"></audio>
     <script src="BGM/Clicksfx.js"></script>
     <audio id="background-music" src=""></audio>
-    <script src="BGM.js"></script>
-    <script src="difficulty.js"></script>
-    <script src="bubble.js"></script>
+    <script src="BGM/BGM.js"></script>
+    <script src="Page1/difficulty.js"></script>
+    <script src="Page1/bubble.js"></script>
 </body>
 </html>
